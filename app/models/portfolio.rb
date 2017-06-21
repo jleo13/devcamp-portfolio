c0050c93958cce63259cb9afc8d91584e1f57a3d
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:25:55 +0000

    Set data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,13 @@ end
   
 scope :abj, -> {where(subtitle: 'My great service, ABJ')}
 
+after_initialize :set_defaults
+
+def set_defaults
+self.main_image ||= "http://via.placeholder.com/600x400" 
+self.thumb_image ||= "http://via.placeholder.com/350x200"
+
+
+end
+
 end
