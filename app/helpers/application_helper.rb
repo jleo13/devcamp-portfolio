55ba3cc2738be091c199668267da77c3c2feb6a7
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 15:50:05 +0000

    Implemented content tage helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def sample_helper
-  "<p>My helper</p>".html_safe
+content_tag(:div, "My content", class: "My class")
   end
   
   def login_helper
@@ -14,5 +14,13 @@ module ApplicationHelper
     end
  
   end
+ 
+ def source_helper(layout_name)
+    if session[:source] 
+   greeting = "Thanks for visiting me from  #{session[:source]} and you are on the #{layout_name} layout "
+  content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+  
   
 end
