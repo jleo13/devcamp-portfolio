19f82a96c33b9eb6b7afb7e5b36b9a1be236b95d
Jordan Hudgens <jordan@devcamp.com>
2017-07-25 15:52:16 +0000

    Implemented refactor for nav and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,19 +16,65 @@ content_tag(:div, "My content", class: "My class")
  
  def source_helper(layout_name)
     if session[:source] 
-   greeting = "Thanks for visiting me from  #{session[:source]} and you are on the #{layout_name} layout "
-  content_tag(:p, greeting, class: "source-greeting")
+     greeting = "Thanks for visiting me from  #{session[:source]} and you are on the #{layout_name} layout "
+     content_tag(:p, greeting, class: "source-greeting")
     end
  end
  
  
  
   def copyright_generator
+      JackLeonardViewTool::Renderer.copyright 'Jack Leonard', 'All rights reserved'
+  end
   
+  def nav_items
+  [
+     {
+        url: root_path,
+        title: 'Home'
+     },
+     
+       {
+        url: about_path,
+        title: 'About Me'
+     } ,
+     
+       {
+        url: contact_path,
+        title: 'Contact'
+     } ,
+     
+       {
+        url: blogs_path,
+        title: 'Blog'
+     } ,
+     
+     {
+        url: portfolios_path,
+        title: 'Portfolio'
+     } ,
+     
+      ]
+  end
+  
+  def nav_helper style, tag_type
 
-JackLeonardViewTool::Renderer.copyright 'Jack Leonard', 'All rights reserved'
+nav_links = ''
+
+nav_items.each do |item|
+    nav_links << "<#{tag_type}><a href= '#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+end
+
+
+
+
+
+
+     nav_links.html_safe
   end
 
-  
+  def active? path
+     "active" if current_page? path 
+  end
   
 end
