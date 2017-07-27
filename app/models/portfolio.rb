7c540f17d90b2bdbed8996df04f7a7b0d06266aa
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 17:55:59 +0000

    refactored placeholder image

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,9 +2,12 @@ class Portfolio < ApplicationRecord
 has_many :technologies
 accepts_nested_attributes_for :technologies, 
                               reject_if: lambda {|attrs| attrs['name'].blank?}
-includes Placeholder
+
 validates_presence_of :title, :body, :main_image, :thumb_image
 
+
+mount_uploader :thumb_image, PortfolioUploader
+mount_uploader :main_image, PortfolioUploader
 def self.by_position
 order("position ASC")
 end
@@ -15,11 +18,8 @@ end
   
 scope :abj, -> {where(subtitle: 'My great service, ABJ')}
 
-after_initialize :set_defaults
 
-      def set_defaults
-          self.main_image ||= Placeholder.image_generator(height: '600', width: '400') 
-         self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-      end
 
+      
+   
 end
